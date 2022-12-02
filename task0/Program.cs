7e582530003c0e316496c9fe84fcc9c92dd8c1f9
VakhrushevDmitry <dimavah@inbox.ru>
2022-12-02 23:58:46 +0300

    добавили все папки в гит

---
+++ task0/Program.cs
@@ -0,0 +1,8 @@
+﻿//Программа, которая на вход принимает число и выдает его квадрат.
+
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(number*number);
+
+int res = number*number;
+Console.WriteLine("квадрат числа: " + res);
---
+++ zad#1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает 2 числа и проверяет, является ли первое число квадратом второго
+
+Console.Write("Введите число 1:");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2:");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a==b*b) 
+{Console.WriteLine("да");}
+else
+{
+    Console.WriteLine("нет");
+}
+
